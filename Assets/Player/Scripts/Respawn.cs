87aabee565b077a8eae2c56be0cbe868621f9ba8
Mateusz Kulpa <m.c.kulpa@gmail.com>
2017-02-26 13:51:54 +0100

    first commit

---
+++ Assets/Player/Scripts/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public Transform respawnable;
+    public Transform spawnPoint;
+    public int minY;
+	// Use this for initialization
+	void Start () {
+        respawnable.position = spawnPoint.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (respawnable.position.y < minY)
+        {
+            respawnable.position = spawnPoint.position;
+        }
+	}
+}
---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private ParticleSystem flames;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (playerMovement.isSliding()) {
+        //          if(flames.isPaused)
+        //          {
+        //              flames.Play();
+        //          }
+        //      } else
+        //      {
+        //          if (flames.isPlaying)
+        //          {
+        //              flames.Stop();
+        //          }
+        //      }
+        //      flames.transform.rotation = Quaternion.LookRotation(playerMovement.getVector().normalized);
+    }
+}
