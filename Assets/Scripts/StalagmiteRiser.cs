0009ef3ec079dc7995dc91a6d4e987de7ee3eb82
Mateusz Kulpa <m.c.kulpa@gmail.com>
2017-02-26 19:57:19 +0100

    improved stalagmite

---
+++ Assets/Scripts/StalagmiteRiser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalagmiteRiser : MonoBehaviour {
+    public Transform stalagmite;
+    public Transform player;
+    public Transform toilet;
+    private float maxDistance = 40;
+    private float maxStalagmiteHeight = -10;
+    private float heightDiff;
+
+    private float initialHeight;
+
+
+
+    // Use this for initialization
+    void Start () {
+        initialHeight = stalagmite.position.y;
+        heightDiff = maxStalagmiteHeight - initialHeight;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = toilet.position.x - player.position.x;
+        float distancePercent = ((maxDistance - distance) / maxDistance);
+
+        float height = initialHeight + distancePercent * heightDiff;
+        if (height < maxStalagmiteHeight)
+        {
+            stalagmite.position = new Vector3(stalagmite.position.x, height, stalagmite.position.z);
+        }
+
+    }
+}
