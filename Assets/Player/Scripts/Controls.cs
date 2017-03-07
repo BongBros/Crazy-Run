3af091afe9cebdb4b8f6c6ae632dca9600353835
Mateusz Kulpa <m.c.kulpa@gmail.com>
2017-03-07 16:44:28 +0100

    * auto run on Q * slide on S * auto run is turned off by left or right input

--- Assets/Player/Scripts/Controls.cs
+++ Assets/Player/Scripts/Controls.cs
@@ -8,6 +8,7 @@ public class Controls : MonoBehaviour
 {
     private Movement movement;
     private bool jump;
+    private bool autorun = true;
     
     private void Awake()
     {
@@ -38,11 +39,29 @@ public class Controls : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            autorun = true;
+        }
+
+        if (CrossPlatformInputManager.GetAxis("Horizontal") != 0) {
+            autorun = false;
+        }
+         
         // Pass all parameters to the character control script.
         PlayerInput playerInput = new PlayerInput();
-        playerInput.slide = Input.GetKey(KeyCode.DownArrow);
+        playerInput.slide = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
         playerInput.jump = jump;
-        playerInput.horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        if (autorun)
+        {
+            playerInput.horizontal = 1;
+        } else
+        {
+            playerInput.horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        }
+
         movement.ProcessInput(playerInput);
         
     }
